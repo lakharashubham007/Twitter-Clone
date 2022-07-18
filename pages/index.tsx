9d557adc8fcfc5c039a4f50815c48a8ecaafc56c
lakharashubham007 <lakharashubham623@gmail.com>
2022-07-18 18:37:48 +0530

    Twitter-Clone-Day1

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Twitter-clone</title>
+      </Head>
+
+      <h1>Hello World I'm in type script mode </h1>
+      
+      
+    </div>
+  )
+}
+
+export default Home
